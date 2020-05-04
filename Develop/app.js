6f7550b5a827dbaeea792f19506f94ca1100f1c3
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-05-04 18:50:08 -0400

    removed duplicate lock json file

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,7 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// Dependencies needed, Link required file paths for classes
+const manager = require("./lib/Manager");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -11,25 +12,138 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
+const employeeList = [];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Initial questions to start creating a new employee, select role type to determine class to apply
+const createTeam = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "Role",
+            message: "Please choose the Employee's role to be added.",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ])
+        .then((res) => {
+            if (res.role === "Manager") { addManager() }
+            else if (res.role === "Engineer") { addEngineer() }
+            else if (res.role === "Intern") { addIntern() }
+        });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// Confirm if you would like to add additional employees to your team. If not, then there are no more prompts and all entries are displayed 
+const addEmployees = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "AdditionalEmployees",
+            message: "Would you like yo add additional employees?"
+        }
+    ]).then((res) => {
+        if (res.AdditionalEmployees === true) { createTeam(); }
+        else if (res.build === false) { displayTeam(); }
+    });
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// Ask manager-specific questions for build
+const addManager = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "Name",
+            message: "Please enter the employee's name."
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Please enter the employee's ID."
+        },
+        {
+            type: "input",
+            name: "Email",
+            message: "Please enter the employee's email address."
+        },
+        {
+            type: "input",
+            name: "OfficeNumber",
+            message: "Please enter the manager's office number."
+        },
+    ]).then(addMan => {
+        const addEmp = new Manager(addMan.Name, addMan.ID, addMan.Email, addMan.OfficeNumber);
+        employeeList.push(addEmp);
+        addEmployees();
+    });
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Add engineer-specific questions for build
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "Name",
+            message: "Please enter the employee's name."
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Please enter the employee's ID."
+        },
+        {
+            type: "input",
+            name: "Email",
+            message: "Please enter the employee's email address."
+        },
+        {
+            type: "input",
+            name: "GitHub Username",
+            message: "Please enter the engineer's GitHub Username."
+        }
+    ]).then(addEngi => {
+        const addEmp = new Manager(addEngi.Name, addEngi.ID, addEngi.Email, addEngi.OfficeNumber);
+        employeeList.push(addEmp);
+        addEmployees();
+    });
+}
+
+// Ask intern-specific questions for build
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "Name",
+            message: "Please enter the employee's name."
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Please enter the employee's ID."
+        },
+        {
+            type: "input",
+            name: "Email",
+            message: "Please enter the employee's email address."
+        },
+        {
+            type: "input",
+            name: "School",
+            message: "Please enter the intern's school."
+        }
+    ]).then(addInt => {
+        const addEmp = new Manager(addInt.Name, addInt.ID, addInt.Email, addInt.OfficeNumber);
+        employeeList.push(addEmp);
+        addEmployees();
+    });
+}
+
+// Display team once manager has confirm that there are no more employees to add 
+const displayTeam = () => {
+    const stringToWrite = render(employees);
+    fs.writeFile(outputPath, stringToWrite, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+createTeam();
