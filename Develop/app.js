536790f47d642d8532da4504dd81ad55c23550ff
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-05-04 23:51:56 -0400

    new

--- Develop/app.js
+++ Develop/app.js
@@ -15,135 +15,148 @@ const render = require("./lib/htmlRenderer");
 // Use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
 const employeeList = [];
 
-// Initial questions to start creating a new employee, select role type to determine class to apply
-const createTeam = () => {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "Role",
-            message: "Please choose the Employee's role to be added.",
-            choices: ["Manager", "Engineer", "Intern"]
-        }
-    ])
-        .then((res) => {
-            if (res.role === "Manager") { addManager() }
-            else if (res.role === "Engineer") { addEngineer() }
-            else if (res.role === "Intern") { addIntern() }
-        });
-}
-
-// Confirm if you would like to add additional employees to your team. If not, then there are no more prompts and all entries are displayed 
-const addEmployees = () => {
-    inquirer.prompt([
-        {
-            type: "confirm",
-            name: "AdditionalEmployees",
-            message: "Would you like yo add additional employees?"
-        }
-    ]).then((res) => {
-        if (res.AdditionalEmployees === true) { createTeam(); }
-        else if (res.build === false) { displayTeam(); }
-    });
-}
+// Initial questions to start creating a new employee, select role type to determine questions to ask
+const roleInquiry = [
+    {
+        type: "list",
+        name: "Role",
+        message: "Please choose the Employee's role to be added.",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+]
 
 // Ask manager-specific questions for build
-const addManager = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "Name",
-            message: "Please enter the employee's name."
-        },
-        {
-            type: "input",
-            name: "ID",
-            message: "Please enter the employee's ID."
-        },
-        {
-            type: "input",
-            name: "Email",
-            message: "Please enter the employee's email address."
-        },
-        {
-            type: "input",
-            name: "OfficeNumber",
-            message: "Please enter the manager's office number."
-        },
-    ]).then(addMan => {
-        const addEmp = new Manager(addMan.Name, addMan.ID, addMan.Email, addMan.OfficeNumber);
-        employeeList.push(addEmp);
-        addEmployees();
-    });
-}
+const managerInfo = [
+    {
+        type: "input",
+        name: "Name",
+        message: "Please enter the employee's name."
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please enter the employee's ID."
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "Please enter the employee's email address."
+    },
+    {
+        type: "input",
+        name: "OfficeNumber",
+        message: "Please enter the manager's office number."
+    },
+]
 
 // Add engineer-specific questions for build
-const addEngineer = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "Name",
-            message: "Please enter the employee's name."
-        },
-        {
-            type: "input",
-            name: "ID",
-            message: "Please enter the employee's ID."
-        },
-        {
-            type: "input",
-            name: "Email",
-            message: "Please enter the employee's email address."
-        },
-        {
-            type: "input",
-            name: "GitHubUsername",
-            message: "Please enter the engineer's GitHub Username."
-        }
-    ]).then(addEngi => {
-        const addEmp = new Manager(addEngi.Name, addEngi.ID, addEngi.Email, addEngi.OfficeNumber);
-        employeeList.push(addEmp);
-        addEmployees();
-    });
-}
+const engineerInfo = [
+    {
+        type: "input",
+        name: "Name",
+        message: "Please enter the employee's name."
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please enter the employee's ID."
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "Please enter the employee's email address."
+    },
+    {
+        type: "input",
+        name: "GitHubUsername",
+        message: "Please enter the engineer's GitHub Username."
+    }
+]
 
 // Ask intern-specific questions for build
-const addIntern = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "Name",
-            message: "Please enter the employee's name."
-        },
-        {
-            type: "input",
-            name: "ID",
-            message: "Please enter the employee's ID."
-        },
-        {
-            type: "input",
-            name: "Email",
-            message: "Please enter the employee's email address."
-        },
-        {
-            type: "input",
-            name: "School",
-            message: "Please enter the intern's school."
+const internInfo = [
+    {
+        type: "input",
+        name: "Name",
+        message: "Please enter the employee's name."
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please enter the employee's ID."
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "Please enter the employee's email address."
+    },
+    {
+        type: "input",
+        name: "School",
+        message: "Please enter the intern's school."
+    }
+]
+
+// Begin asking question to determine role of employee to be added and questions to be asked
+const init = () => {
+    inquirer.prompt(roleInquiry).then(function (answers) {
+        if (answers.Role === "Manager") {
+            addManager();
         }
-    ]).then(addInt => {
-        const addEmp = new Manager(addInt.Name, addInt.ID, addInt.Email, addInt.OfficeNumber);
-        employeeList.push(addEmp);
-        addEmployees();
-    });
+        else if (answers.roleInquiry === "Engineer") {
+            addEngineer();
+        }
+        else if (answers.roleInquiry === "Intern") {
+            addIntern();
+        } else {
+            displayTeam(render(employeeList));
+        }
+    })
 }
 
-// Display team once manager has confirm that there are no more employees to add 
-const displayTeam = () => {
-    const stringToWrite = render(employees);
-    fs.writeFile(outputPath, stringToWrite, function (err) {
+// Ask Manager questions and create manager
+function addManager() {
+    inquirer.prompt(managerInfo).then(function (answers) {
+        const newMgmt = new Manager(answers.name, answers.ID, answers.Email, answers.OfficeNumber);
+        employeeList.push(newManager);
+        init();
+    })
+};
+
+// Ask engineer questions and create engineer
+function addEngineer() {
+    inquirer.prompt(engineerInfo).then(function (answers) {
+        const newEng = new Engineer(answers.name, answers.ID, answers.Email, answers.GitHubUsername);
+        employeeList.push(newEng);
+        init();
+    })
+};
+
+// Ask intern questions and create intern
+function addIntern() {
+    inquirer.prompt(internInfo).then(function (answers) {
+        const newInt = new Intern(answers.name, answers.ID, answers.Email, answers.School);
+        employeeList.push(newInt);
+        init();
+    })
+};
+
+
+function buildTeam() {
+    fs.writeFile(outputPath, render(teamMembers), function (err) {
         if (err) {
-            throw err;
+            console.log(err);
         }
-    });
+    })
 }
 
-createTeam();
+function myFunc() {
+    fs.writeFile(outputPath, render(teamMembers), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
+
+// Start the prompt
+init();
